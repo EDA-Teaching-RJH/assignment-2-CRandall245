474aec9bb1d4d19d7d0cfea229b8d2646b745bf0
CRandall245 <ce327@kent.ac.uk>
2024-10-22 15:20:44 +0000

    Task 2 complete

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,11 @@
 ### Part Two -- your code goes here. 
+import random
+
+def main():
+    goal = int(random.uniform(1,10))
+    user = int(input("Guess the random number:"))
+    while user != goal:
+        user = int(input("Guess the random number:"))
+
+
+main()
